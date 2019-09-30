9508a78229564e26b971c2b3adab00d60a29b52d
Jinwoo Jeong <jjw8967>
2019-09-30 14:39:19 +0900

    Convert the render format from Canvas to SVG

--- pdf.js_practice/js/main.js
+++ pdf.js_practice/js/main.js
@@ -6,11 +6,9 @@ let pdfDoc = null,
     pageIsRendering = false,
     pageNumIsPending = null;
 
-
-//렌더링 할 pdf크기 지정 (ex 1.5면 화면에서 크게 보이고, 0.5면 작게 축소되어 보임)
-const scale = 1,
-    canvas = document.querySelector('#pdf-render'),
-    ctx = canvas.getContext('2d');
+const svg = document.getElementById('pdf-render')
+    , container = document.getElementById('container')
+    , scale = 1.5;
 
 // 페이지 렌더링하는거 
 const renderPage = num =>{
@@ -20,22 +18,18 @@ const renderPage = num =>{
     pdfDoc.getPage(num).then(page => {
         //set scale
         const viewport = page.getViewport({scale}); //페이지 크기 정보 가져오는거
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
+        svg.height = viewport.height;
+        svg.width = viewport.width;
 
-        const renderCtx = {
-            canvasContext:ctx,
-            viewport
-        } //renderContext객체 생성
+        page.getOperatorList()    
+            .then(function (opList) {
+            var svgGfx = new pdfjsLib.SVGGraphics(page.commonObjs, page.objs);
+            return svgGfx.getSVG(opList, viewport);
+            })
+            .then(function (svg) {
+            container.appendChild(svg);
+            });
 
-        page.render(renderCtx).promise.then(() =>{
-            pageIsRendering = false;
-
-            if(pageNumIsPending !== null){
-                renderPage(pageNumIsPending);
-                pageNumIsPending = null;
-            }
-        });
         //현재페이지 보여주는거
         document.querySelector('#page-num').textContent = num; 
     });
@@ -71,34 +65,38 @@ const showNextPage = () =>{
 
 }
 
+
+
 let inputElement = document.getElementById('input_file')
 inputElement.onchange = function(event) {
 
-    var file = event.target.files[0];
+    let file = event.target.files[0];
 
     //Step 2: Read the file using file reader
-    var fileReader = new FileReader();
+    let fileReader = new FileReader();
 
     fileReader.onload = function() {
 
         //Step 4:turn array buffer into typed array
-        var typedarray = new Uint8Array(this.result);
+        let typedarray = new Uint8Array(this.result);
 
         //Step 5:PDFJS should be able to read this
-        pdfjsLib.getDocument(typedarray).promise.then(pdfDoc_ =>{
-		      pdfDoc = pdfDoc_;
-			  document.querySelector('#page-count').textContent = pdfDoc.numPages;
+        let loadingTask = pdfjsLib.getDocument(typedarray)
 
-			  renderPage(pageNum);
+        
 
-		});
-		
+        loadingTask.promise.then((pdfDocument)=>{
+            pdfDoc = pdfDocument
+            document.querySelector('#page-count').textContent = pdfDocument.numPages;
+            renderPage(pageNum);
+        })
 
     };
     //Step 3:Read the file as ArrayBuffer
     fileReader.readAsArrayBuffer(file);
 
  }
+
 
 
 //버튼 이벤트
