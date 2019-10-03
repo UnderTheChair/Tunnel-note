9d0f8abb46d5028faa514a162d9c031751462a34
jinu <jjw8967@ajou.ac.kr>
2019-10-03 21:36:52 +0900

    Implement simple Socket.io & pdf upload & track mouse event

---
+++ mobile/swift/Pods/Target Support Files/Pods-TunnelNote-TunnelNoteUITests/Pods-TunnelNote-TunnelNoteUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TunnelNote_TunnelNoteUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TunnelNote_TunnelNoteUITests
+@end
---
+++ mobile/swift/TunnelNoteTests/TunnelNoteTests.swift
@@ -0,0 +1,34 @@
+//
+//  TunnelNoteTests.swift
+//  TunnelNoteTests
+//
+//  Created by 정진우 on 21/09/2019.
+//  Copyright © 2019 정진우. All rights reserved.
+//
+
+import XCTest
+@testable import TunnelNote
+
+class TunnelNoteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
