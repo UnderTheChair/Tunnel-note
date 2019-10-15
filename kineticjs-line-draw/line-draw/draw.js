33f3ae82a9de126b56f42037dd0cfe0b7e793b82
jinu <jjw8967@ajou.ac.kr>
2019-10-15 14:23:29 +0900

    Change code logic deleting 'window.requestAnimFrame' in line-draw/draw.js file

--- kineticjs-line-draw/line-draw/draw.js
+++ kineticjs-line-draw/line-draw/draw.js
@@ -1,158 +1,139 @@
-(function() {
-	
-	// Get a regular interval for drawing to the screen
-	window.requestAnimFrame = (function (callback) {
-		return window.requestAnimationFrame || 
-					window.webkitRequestAnimationFrame ||
-					window.mozRequestAnimationFrame ||
-					window.oRequestAnimationFrame ||
-					window.msRequestAnimaitonFrame ||
-					function (callback) {
-					 	window.setTimeout(callback, 1000/60);
-					};
-	})();
+// Set up the canvas
+let canvas = document.getElementById("sig-canvas");
+let ctx = canvas.getContext("2d");
 
-	// Set up the canvas
-	var canvas = document.getElementById("sig-canvas");
-	var ctx = canvas.getContext("2d");
+let mode = "pen";
+
+// Set up mouse events for drawing
+let drawing = false;
+let mousePos = { x:0, y:0 };
+let lastPos = mousePos;
+
+// Set up the UI
+let sigText = document.getElementById("sig-dataUrl");
+let sigImage = document.getElementById("sig-image");
+let clearBtn = document.getElementById("sig-clearBtn");
+let penBtn = document.getElementById("sig-penBtn");
+let eraserBtn = document.getElementById("sig-eraserBtn");
+let submitBtn = document.getElementById("sig-submitBtn");
+
+clearBtn.addEventListener("click", function (e) {
+	clearCanvas();
+	sigText.innerHTML = "Data URL for your signature will go here!";
+	sigImage.setAttribute("src", "");
+}, false);
+
+penBtn.addEventListener("click", function(e) {
 	ctx.strokeStyle = "blue";
 	ctx.lineWith = 2;
+	mode = "mode";
+}, false);
 
-    var mode = "pen";
-    $("#sig-penBtn").click(function(){ mode="pen"; });
-    $("#sig-eraserBtn").click(function(){ mode="eraser"; });
-	// Set up the UI
-	var sigText = document.getElementById("sig-dataUrl");
-	var sigImage = document.getElementById("sig-image");
-    var clearBtn = document.getElementById("sig-clearBtn");
-    var penBtn = document.getElementById("sig-penBtn");
-    var eraserBtn = document.getElementById("sig-eraserBtn");
-	var submitBtn = document.getElementById("sig-submitBtn");
-	clearBtn.addEventListener("click", function (e) {
-		clearCanvas();
-		sigText.innerHTML = "Data URL for your signature will go here!";
-		sigImage.setAttribute("src", "");
-    }, false);
-    penBtn.addEventListener("click", function(e) {
-        ctx.strokeStyle = "blue";
-        mode = "mode";
-    }, false);
-    eraserBtn.addEventListener("click", function (e) {
-        // ctx.strokeStyle = "white";
-        mode = "eraser";
-    }, false);
-	submitBtn.addEventListener("click", function (e) {
-		var dataUrl = canvas.toDataURL();
-		sigText.innerHTML = dataUrl;
-		sigImage.setAttribute("src", dataUrl);
-	}, false);
+//TODO : Not Working
+eraserBtn.addEventListener("click", function (e) {
+	// ctx.strokeStyle = "white";
+	mode = "eraser";
+}, false);
 
-	// Set up mouse events for drawing
-	var drawing = false;
-	var mousePos = { x:0, y:0 };
-	var lastPos = mousePos;
-	canvas.addEventListener("mousedown", function (e) {
-		drawing = true;
-		lastPos = getMousePos(canvas, e);
-	}, false);
-	canvas.addEventListener("mouseup", function (e) {
-		drawing = false;
-	}, false);
-	canvas.addEventListener("mousemove", function (e) {
-		mousePos = getMousePos(canvas, e);
-	}, false);
+submitBtn.addEventListener("click", function (e) {
+	let dataUrl = canvas.toDataURL();
+	sigText.innerHTML = dataUrl;
+	sigImage.setAttribute("src", dataUrl);
+}, false);
 
-	// Set up touch events for mobile, etc
-	canvas.addEventListener("touchstart", function (e) {
-		mousePos = getTouchPos(canvas, e);
-		var touch = e.touches[0];
-		var mouseEvent = new MouseEvent("mousedown", {
-			clientX: touch.clientX,
-			clientY: touch.clientY
-		});
-		canvas.dispatchEvent(mouseEvent);
-	}, false);
-	canvas.addEventListener("touchend", function (e) {
-		var mouseEvent = new MouseEvent("mouseup", {});
-		canvas.dispatchEvent(mouseEvent);
-	}, false);
-	canvas.addEventListener("touchmove", function (e) {
-		var touch = e.touches[0];
-		var mouseEvent = new MouseEvent("mousemove", {
-			clientX: touch.clientX,
-			clientY: touch.clientY
-		});
-		canvas.dispatchEvent(mouseEvent);
-	}, false);
-
-	// Prevent scrolling when touching the canvas
-	document.body.addEventListener("touchstart", function (e) {
-		if (e.target == canvas) {
-			e.preventDefault();
+let mousePenEvent = {
+	mousedown(e){
+		if(mode == "pen"){
+			drawing = true;
+			lastPos = getMousePos(canvas,e);
 		}
-	}, false);
-	document.body.addEventListener("touchend", function (e) {
-		if (e.target == canvas) {
-			e.preventDefault();
+	},
+	mouseup(e){
+		if (mode == "eraser"){
+			eraserCanvas();
+		}else if (mode == "pen"){
+			drawing = false;
+			eraserCanvas();
 		}
-	}, false);
-	document.body.addEventListener("touchmove", function (e) {
-		if (e.target == canvas) {
-			e.preventDefault();
+	},
+	mousemove(e){
+		if (mode == "eraser"){
+			eraserCanvas();	
 		}
-	}, false);
-
-	// Get the position of the mouse relative to the canvas
-	function getMousePos(canvasDom, mouseEvent) {
-		var rect = canvasDom.getBoundingClientRect();
-		return {
-			x: mouseEvent.clientX - rect.left,
-			y: mouseEvent.clientY - rect.top
-		};
-	}
-
-	// Get the position of a touch relative to the canvas
-	function getTouchPos(canvasDom, touchEvent) {
-		var rect = canvasDom.getBoundingClientRect();
-		return {
-			x: touchEvent.touches[0].clientX - rect.left,
-			y: touchEvent.touches[0].clientY - rect.top
-		};
-	}
-
-	// Draw to the canvas
-	function renderCanvas() {
-		if (drawing) {
-			ctx.moveTo(lastPos.x, lastPos.y);
-			ctx.lineTo(mousePos.x, mousePos.y);
-			ctx.stroke();
-			lastPos = mousePos;
+		else if (mode = "pen"){
+			mousePos = getMousePos(canvas, e);
+			renderCanvas();
 		}
+
 	}
-    function eraserCanvas(){
-        if(mode == "eraser"){
-            // ctx.globalCompositeOperation = "destination-out";  
-            ctx.fillStyle = 'white';
-            ctx.strokeStyle = 'rgba(0,0,0,1)';  
-        }
-        
-    }
-	function clearCanvas() {
-		canvas.width = canvas.width;
+}
+
+function enablePenEventListener(){
+	canvas.addEventListener("mousedown", mousePenEvent.mousedown, false);
+	canvas.addEventListener("mouseup", mousePenEvent.mouseup, false);
+	canvas.addEventListener("mousemove", mousePenEvent.mousemove, false);
+};
+
+enablePenEventListener();
+
+// Set up touch events for mobile, etc
+canvas.addEventListener("touchstart", function (e) {
+	mousePos = getTouchPos(canvas, e);
+	let touch = e.touches[0];
+	let mouseEvent = new MouseEvent("mousedown", {
+		clientX: touch.clientX,
+		clientY: touch.clientY
+	});
+	canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+	let mouseEvent = new MouseEvent("mouseup", {});
+	canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+	let touch = e.touches[0];
+	let mouseEvent = new MouseEvent("mousemove", {
+		clientX: touch.clientX,
+		clientY: touch.clientY
+	});
+	canvas.dispatchEvent(mouseEvent);
+}, false);
+
+// Get the position of the mouse relative to the canvas
+function getMousePos(canvasDom, mouseEvent) {
+	let rect = canvasDom.getBoundingClientRect();
+	return {
+		x: mouseEvent.clientX - rect.left,
+		y: mouseEvent.clientY - rect.top
+	};
+}
+
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+	let rect = canvasDom.getBoundingClientRect();
+	return {
+		x: touchEvent.touches[0].clientX - rect.left,
+		y: touchEvent.touches[0].clientY - rect.top
+	};
+}
+
+// Draw to the canvas
+function renderCanvas() {
+	if (drawing) {
+		ctx.moveTo(lastPos.x, lastPos.y);
+		ctx.lineTo(mousePos.x, mousePos.y);
+		ctx.stroke();
+		lastPos = mousePos;
 	}
+}
 
+// TODO : Not Working
+function eraserCanvas(){
+	// ctx.globalCompositeOperation = "destination-out";  
+	ctx.fillStyle = 'white';
+	ctx.strokeStyle = 'rgba(0,0,0,1)';  
+}
 
-	// Allow for animation
-	(function drawLoop () {
-		requestAnimFrame(drawLoop);
-        if(mode = "pen"){
-           renderCanvas(); 
-        }
-        
-        if(mode = "eraser"){
-            eraserCanvas();
-        }
-        
-	})();
-
-})();
+function clearCanvas() {
+	canvas.width = canvas.width;
+}
