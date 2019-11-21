21232a2116e7977fe551e5c4fa9aafac24c91505
jinu <jjw8967@ajou.ac.kr>
2019-11-21 18:34:05 +0900

    Release blocking people at socket.io

--- back/routers/draw_socket.js
+++ back/routers/draw_socket.js
@@ -2,49 +2,30 @@ module.exports = (io) => {
   io.sockets.on('connection', (socket) => {
 
     socket.join('draw');
-    let room = io.sockets.adapter.rooms['draw'];
-    if (room.length > 2) {
-      socket.leave('draw');
-      socket.disconnect;
-      return;
-    }
 
     socket.on('SEND_MESSAGE', (data) => {
-      if (room.length > 2) {
-        return;
-      }
-      io.sockets.emit('SENDED_MESSAGE', data); //broadcast
+        io.sockets.emit('SENDED_MESSAGE',data); //broadcast
     });
-
-    socket.on('MOUSEDOWN', (data) => {
-      if (room.length > 2) {
-        return;
-      }
-      socket.broadcast.to('draw').emit('MOUSEDOWN', data);
+  
+    socket.on('MOUSEDOWN',(data) => {
+      socket.broadcast.to('draw').emit('MOUSEDOWN',data);
     })
-
-    socket.on('MOUSEUP', () => {
+  
+    socket.on('MOUSEUP',() => {
       socket.broadcast.to('draw').emit('MOUSEUP');
     })
-
-    socket.on('MOUSEMOVE', (data) => {
-      if (room.length > 2) {
-        return;
-      }
-      socket.broadcast.to('draw').emit('MOUSEMOVE', data);
+  
+    socket.on('MOUSEMOVE',(data) => {
+      socket.broadcast.to('draw').emit('MOUSEMOVE',data);
     })
 
     socket.on('SETUP', () => {
-      if (room.length > 2) {
-        return;
-      }
       socket.broadcast.to('draw').emit('SETUP');
     })
-
-    socket.on('DISCONNECT', () => {
+  
+    socket.on('DISCONNECT',()=>{
       socket.leave('draw');
       socket.disconnect();
     })
   });
 }
-
