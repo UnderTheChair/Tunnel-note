3027a686eb707a79c7533e3a7d538568cbfe4a29
jinu <jjw8967@ajou.ac.kr>
2019-11-22 21:00:48 +0900

    Apply adding thumnail base64 to pdf schema in MongoDB when upload pdf

--- back/routers/pdfs.js
+++ back/routers/pdfs.js
@@ -5,6 +5,25 @@ const userModel = require('../db/models/user');
 const authMiddleware = require('../middlewares/auth')
 const multer = require('multer');
 const fs = require('fs');
+const PDF2Pic = require("pdf2pic");
+ 
+const pdf2pic = new PDF2Pic({
+  density: 100,           // output pixels per inch
+  savename: "untitled",   // output file name
+  savedir: "temp",    // output file location
+  format: "png",          // output file format
+  size: "600x600"         // output size in pixels
+});
+
+/**
+ * Use for pdf2pic
+ * 
+ * Ubuntu: sudo apt-get install imagemagick ghostscript poppler-utils
+ * OS X : brew install imagemagick ghostscript poppler
+ * 
+ */
+
+//brew install imagemagick graphicsmagick
 
 // Set file upload storage
 const storage = multer.diskStorage({
@@ -30,24 +49,41 @@ router.post('/upload', (req, res) => {
   let path = '/temp/' + req.decoded;
   let userData = { email: req.decoded };
   let pdfData = { name: name, size: size, path: path };
-
-  userModel.findOne(userData).then((user) => {
+  let pdfPath = __dirname + '/..' + path + '/' + name;
+  
+  console.log(pdfPath);
+  pdf2pic.convertToBase64(pdfPath, 1).then((resolve) => {
+    if (resolve.base64) {
+      console.log("image converter successfully!");
+   
+      // assuming you're using some ORM to save base64 to db
+      pdfData['thumbnail'] = resolve.base64;
+      return resolve;
+    } else return resolve;
+  })
+  .then(() => {
+    return userModel.findOne(userData);
+  })
+  .then((user) => {
     pdfData['user'] = { _id: user._id };
 
     pdfModel.create(pdfData).then((pdf) => {
       user['pdf_list'] = [...user['pdf_list'], { _id: pdf._id }];
-      userModel.updateOne(userData, user).then(() => {
-        res.send(pdf);
-      })
+      return userModel.updateOne(userData, user)
     })
-  }).catch((err) => {
+  })
+  .then(() => {
+    res.send({data: "ok", name: name});
+  })
+  .catch((err) => {
+    console.log(err);
     res.send(err);
   })
 
 })
 
 router.get('/', (req, res) => {
-  let userData = {email: req.decoded};
+  let userData = { email: req.decoded };
 
   userModel.findOne(userData).populate('pdf_list').exec((err, data) => {
     res.send(data.pdf_list);
