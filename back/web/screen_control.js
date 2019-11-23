2c8a0c5579e73c94deccf59cdcac63ba6d963373
HongJow <hoongjow@gmail.com>
2019-11-23 17:43:52 +0900

    Synchronize mobile with pc tunnel box resize

--- back/web/screen_control.js
+++ back/web/screen_control.js
@@ -7,6 +7,14 @@ let screenControl = function() {
     setScrollLeft(offsetLeft) {
       document.querySelector("#viewerContainer").scrollLeft = offsetLeft;
       document.querySelector("#viewerContainer").dispatchEvent(new Event('scroll'))
+    },
+    setScrollWidth(offsetWidth) {
+      document.querySelector("#viewerContainer").style.offsetWidth = offsetWidth;
+      document.querySelector("#viewerContainer").dispatchEvent(new Event('scroll'))
+    },
+    setScrollHeight(offsetHeight) {
+      document.querySelector("#viewerContainer").style.offsetHeight = offsetHeight + 64;
+      document.querySelector("#viewerContainer").dispatchEvent(new Event('scroll'))
     }
   }
 }()
