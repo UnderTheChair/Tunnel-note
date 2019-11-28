ca7800b27e2d5dd3af960d3051b8e259ddad63bc
HongJow <hoongjow@gmail.com>
2019-11-28 20:10:08 +0900

    Fix bug for tunnel box resize

--- back/routers/tunnel_box_socket.js
+++ back/routers/tunnel_box_socket.js
@@ -3,12 +3,9 @@ module.exports = (io) => {
   io.sockets.on('connection', (socket)=>{
     socket.join('tunnel_box');
     
+    //pc -> mobile
     socket.on('BOX_INIT', (position) => {
       io.sockets.emit('BOX_INIT', position); //broadcast
-    });
-
-    socket.on('BOX_SIZE_INIT', (sizeData) => {
-      socket.broadcast.to('tunnel_box').emit('BOX_SIZE_INIT', sizeData);
     });
 
     socket.on('BOX_MOVE', (position) => {
@@ -26,6 +23,20 @@ module.exports = (io) => {
     socket.on('BOX_DOWN', (position) => {
       socket.broadcast.to('tunnel_box').emit('BOX_DOWN');
     });
+
+    //mobile -> pc
+    socket.on('BOX_SIZE_INIT', (sizeData) => {
+      socket.broadcast.to('tunnel_box').emit('BOX_SIZE_INIT', sizeData);
+    });
+
+    socket.on('MOBILE_MOVE', (position) => {
+      socket.broadcast.to('tunnel_box').emit('MOBILE_MOVE', position);
+    });
+      
+    socket.on('MOBILE_RESIZE', (position) => {
+      socket.broadcast.to('tunnel_box').emit('MOBILE_RESIZE', position);
+    });
+      
     
     socket.on('DISCONNECT', ()=>{
       socket.broadcast.to('tunnel_box').emit('DISCONNECT');
