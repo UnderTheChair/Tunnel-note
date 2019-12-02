c16d1c860ad123318eeccdb1de06ea7a704f6e4e
Jinwoo Jeong <jjw8967@ajou.ac.kr>
2019-12-02 11:21:42 +0000

    Add loading bar generated by entering the pdf in home page

--- front/src/components/PDFItem.vue
+++ front/src/components/PDFItem.vue
@@ -4,25 +4,36 @@
       <img v-bind:src="getThumbnail()" width="250" height="250" style="margin: auto;" />
       <h4>{{pdfName}}</h4>
     </div>
+    <loading :active.sync="isLoading" 
+        :can-cancel="true" 
+        :on-cancel="onCancel"
+        :is-full-page="fullPage"/>
   </div>
 </template>
 
 <script>
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+
 export default {
   name: "PDFItem",
   data() {
     return{
       baseURL: this.$store.getters.getBaseUrl,
+      isLoading: false,
+      fullPage: true,
     }
   },
-  computed: {},
+  components: {
+    Loading
+  },
   methods: {
     getThumbnail: function() {
       let srcValue = `data:image/png;base64,${this.thumbnail}`;
       return srcValue;
     },
     movePDF: function() {
-      
+      this.isLoading = true;
       this.$http.post(`${this.baseURL}/pdfs/blob`, {
         pdfId: this.pidId,
         pdfName: this.pdfName
@@ -40,12 +51,15 @@ export default {
         localStorage.setItem('fileURL', fileURL)
         localStorage.setItem('pdfName', this.pdfName)
         window.open("http://13.125.136.140/web/")
-
+        this.isLoading = false
       })
       .catch(error => {
         console.log(error);
       });
       //localStorage.setItem("pdf-file",  )
+    },
+    onCancel() {
+      console.log('User cancelled the loader.')
     }
   },
   props: ["pdfName", "thumbnail", "pddId"]
